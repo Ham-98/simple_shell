b422ea69b9fa37e8074562c151ed96f4d6d6fb98
Ham-98 <hkkarani922@gmail.com>
2022-05-18 14:05:46 -0700

    file 1

--- built_ins_help_two.c
+++ built_ins_help_two.c
@@ -1,45 +1,127 @@
-#include "shell.h"
-
-void help_env(void);
-void help_setenv(void);
-void help_unsetenv(void);
-void help_history(void);
+#include "main.h"
 
 /**
- * help_env - Displays information on the shellby builtin command 'env'.
+ * create_env - creates a list of environment variables
+ * @env: array of environment variables
+ *
+ * Return: new head of list. Otherwise NULL
  */
-void help_env(void)
+list_t *create_env(char **env)
 {
-	char *msg = "env: env\n\tPrints the current environment.\n";
+	unsigned int i;
+	char *name, *value;
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	if (env == NULL)
+		return (NULL);
+
+	for (i = 0; env[i]; i++)
+	{
+		name = _strtok(env[i], "=");
+		value = _strtok(NULL, "\n");
+
+		if (add_node_end(&env_list, name, value) == NULL)
+			return (NULL);
+	}
+
+	return (env_list);
 }
 
 /**
- * help_setenv - Displays information on the shellby builtin command 'setenv'.
+ * print_env - prints all the environment variables
  */
-void help_setenv(void)
+void print_env(void)
 {
-	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+	list_t *trav = env_list;
+	/*unsigned int index = 0;*/
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environment variable, or modifies an existing one.\n\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\tUpon failure, prints a message to stderr.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	while (trav != NULL)
+	{
+		printf("%s=", trav->name);
+		printf("%s\n", trav->value);
+
+		trav = trav->next;
+	}
 }
 
 /**
- * help_unsetenv - Displays information on the shellby builtin command
- * 'unsetenv'.
+ * _getenv - gets the value of an environment variable
+ * @name: environment variable
+ *
+ * Return: value of name. Otherwise NULL
  */
-void help_unsetenv(void)
+char *_getenv(const char *name)
 {
-	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+	list_t *trav = env_list;
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environmental variable.\n\n\tUpon failure, prints a ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "message to stderr.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	if (name == NULL)
+		return (NULL);
+
+	while (trav && _strcmp(trav->name, name)) /* check each node for a match */
+		trav = trav->next;
+
+	if (trav)
+		return (trav->value);
+
+	return (NULL);
+}
+
+/**
+ * _setenv - changes or adds an environment variable
+ * @name: name of environment variable
+ * @value: value of environment variable
+ * @overwrite: overwrite flag
+ *
+ * Return: 0 if successful. Otherwise -1
+ */
+int _setenv(const char *name, const char *value, int overwrite)
+{
+	int index;
+
+	if (name == NULL || value == NULL || is_in_str(name, '='))
+		return (-1);
+
+	/* find index of node with name */
+	index = find_name(env_list, name);
+
+	/* if name not in list */
+	if (index == -1)
+	{
+		if (add_node_end(&env_list, name, value) == NULL)
+			return (-1);
+
+		return (0);
+	}
+
+	/* update node at index */
+	if (overwrite)
+		return (update_value(env_list, index, value));
+
+	return (0);
+}
+
+/**
+ * _unsetenv - deletes the variable name from the environment
+ * @name: name of variable to delete
+ *
+ * Return: 0 success. 1 otherwise
+ */
+int _unsetenv(const char *name)
+{
+	int index;
+
+	if (name == NULL)
+		return (-1);
+
+	/* find index of node to delete */
+	index = find_name(env_list, name);
+
+	/* if name is not in the list */
+	if (index == -1)
+		return (0);
+
+	/* delete node at index */
+	if (delete_node_index(&env_list, index) == -1)
+		return (-1);
+
+	return (0);
 }
